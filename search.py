426c020a898eb5d6b69479439d0e52d1a12ba2e1
Eric Cramer <Eric.Cramer@sony.com>
2018-05-18 16:50:40 -0700

    Added search.py

---
+++ search.py
@@ -0,0 +1,10 @@
+"""Depth first search and A-Star"""
+
+class Problem(object):
+    """Represents a problem.
+
+    Subclass this with your own problem.
+    """
+
+    def get_initial_state(self):
+        pass
